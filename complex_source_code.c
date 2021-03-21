d3f7bdc6d9034c2c3f94897fcea6a2d0ea93cd5a
Swapnil Khandekar <42568338+swapnilkhandekar999@users.noreply.github.com>
2021-03-21 21:38:17 +0530

    Update complex_source_code.c

--- complex_source_code.c
+++ complex_source_code.c
@@ -16,8 +16,23 @@ int main(){
   num2.real=2;
   num2.imaginary=3;
   
-  sum(complex_t,complex_t);
-  difference(complex_t,complex_t);
-  multiplication(complex_t,complex_t);
-  division(complex_t,complex_t);
+  sum(num1,num2);
+  difference(num1,num2);
+  multiplication(num1,num2);
+  division(num1,num2);
 }
+
+complex_t sum(complex_t,complex_t){
+  complex_t result;
+  result.real=num1.real+num2.real;
+  result.imaginary=num1.imaginary+num2.imaginary;
+  return result;
+}
+complex_t difference(complex_t,complex_t){
+  complex_t result;
+  result.real=num1.real-num2.real;
+  result.imaginary=num1.imaginary-num2.imaginary;
+  return result;
+}
+complex_t multiplication(complex_t,complex_t);
+complex_t division(complex_t,complex_t);
