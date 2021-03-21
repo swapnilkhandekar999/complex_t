18fd54ac8e93d5aee9f2fac3f4e1fee7e3649bef
Swapnil Khandekar <42568338+swapnilkhandekar999@users.noreply.github.com>
2021-03-21 21:43:36 +0530

    Update complex_source_code.c

--- complex_source_code.c
+++ complex_source_code.c
@@ -10,16 +10,20 @@ complex_t multiplication(complex_t,complex_t);
 complex_t division(complex_t,complex_t);
 
 int main(){
-  complex_t num1,num2;
+  complex_t num1,num2,res[4];
   num1.real=1;
   num1.imaginary=1;
   num2.real=2;
   num2.imaginary=3;
   
-  sum(num1,num2);
-  difference(num1,num2);
-  multiplication(num1,num2);
-  division(num1,num2);
+  res[0]=sum(num1,num2);
+  res[1]=difference(num1,num2);
+  res[2]=multiplication(num1,num2);
+  res[3]=division(num1,num2);
+  
+  for(int i=0;i<4;i++){
+  printf("%f%f",res[i].real,res[i].imaginary);
+  }
 }
 
 complex_t sum(complex_t,complex_t){
