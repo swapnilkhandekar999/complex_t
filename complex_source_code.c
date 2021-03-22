b7f87d3dfb81038421d8622aa895276fff08a776
Swapnil Khandekar <42568338+swapnilkhandekar999@users.noreply.github.com>
2021-03-22 12:47:38 +0530

    Update complex_source_code.c

--- complex_source_code.c
+++ complex_source_code.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
-#include "complex.h"
 typedef struct complex_t {
 float real;
 float imaginary;
-} complex_t
+} complex_t;
 complex_t sum(complex_t,complex_t);
 complex_t difference(complex_t,complex_t);
 complex_t multiplication(complex_t,complex_t);
@@ -26,25 +25,25 @@ int main(){
   }
 }
 
-complex_t sum(complex_t,complex_t){
+complex_t sum(complex_t num1,complex_t num2){
   complex_t result;
   result.real=num1.real+num2.real;
   result.imaginary=num1.imaginary+num2.imaginary;
   return result;
 }
-complex_t difference(complex_t,complex_t){
+complex_t difference(complex_t num1,complex_t num2){
   complex_t result;
   result.real=num1.real-num2.real;
   result.imaginary=num1.imaginary-num2.imaginary;
   return result;
 }
-complex_t multiplication(complex_t,complex_t){
+complex_t multiplication(complex_t num1,complex_t num2){
   complex_t result;
   result.real=(num1.real*num2.real)-(num1.imaginary*num2.imaginary);
   result.imaginary=(num1.real*num2.imaginary)+(num2.real*num1.imaginary);
   return result;
 }
-complex_t division(complex_t,complex_t){
+complex_t division(complex_t num1,complex_t num2){
   complex_t result;
   result.real=((num1.real*num2.real)+(num1.imaginary*num2.imaginary))/(num2.real*num2.real+num2.imaginary*num2.imaginary);
   result.imaginary=((num2.real*num1.imaginary)-(num1.real*num2.imaginary))/(num2.real*num2.real+num2.imaginary*num2.imaginary);
