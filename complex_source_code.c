4925d1142fd3b5e3f04425980dc5cfff5114b4be
swapnilkhandekar999 <swapnilkhandekar999@gmail.com>
2021-03-26 17:38:01 +0530

    removed scanf

--- complex_source_code.c
+++ complex_source_code.c
@@ -12,7 +12,7 @@ complex_t division(complex_t,complex_t);
 
 int main(){
   complex_t num1,num2,res[4];
-  scanf("%d",&num1.real);
+  num1.real=1;
   num1.imaginary=1;
   num2.real=2;
   num2.imaginary=3;
